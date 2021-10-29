21b71782ace489eddf47e3ce32a9173732148f99
yuanpeng <787417043@qq.com>
2021-10-29 23:17:19 +0800

    day1：基本数据类型

--- src/cn/Hello.java
+++ src/cn/Hello.java
@@ -5,6 +5,29 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("111221212hello");
+		
+		/**
+		 * 111111
+		 */
+		int a = 10;
+		
+		System.out.println(a++);
+		System.out.println(777);
+		//八进制
+		System.out.println(0777);
+		System.out.println(010);
+		//十六进制
+		System.out.println(0x10);
+		//二进制
+		System.out.println(0b111);
+		//长整型
+		System.out.println(777L);
+		//浮点型
+		System.out.println(0.9f-0.6f);
+		System.out.println(0.9-0.6);
+		//布尔型boolean
+		System.out.println(true);
+		System.out.println(false);
 	}
 
 }
